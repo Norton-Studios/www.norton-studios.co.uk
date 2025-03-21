a7bd48fe2999052300f4afe8de0b0200107463b0
Linus Norton <linusnorton@gmail.com>
2025-03-21 10:58:18 +0000

    overview page content

--- src/app/case-studies/page.tsx
+++ src/app/case-studies/page.tsx
@@ -1,13 +1,28 @@
 import { Container } from '@/components/Container';
 import { Heading } from '../../components/Heading';
+import fs from "node:fs";
+import { compileMDX } from "next-mdx-remote/rsc";
 
-export default function CaseStudies() {
+export default async function CaseStudies() {
+  const caseStudies = fs
+    .readdirSync('src/content/case-studies')
+    .filter((file) => file.endsWith('.mdx'))
+    .map((file) => compileMDX<{ title: string }>({
+      source: fs.readFileSync(`src/content/case-studies/${file}`, 'utf-8'),
+      options: { parseFrontmatter: true }
+    }));
+
+  const caseStudiesContent = await Promise.all(caseStudies);
+
   return (
     <div>
       <Container>
-        <Heading level="h1" className="text-center sm:text-left">
-          Case Studies
-        </Heading>
+        { caseStudiesContent.map(({ content, frontmatter }) => (
+        <div key={frontmatter.title}>
+          <Heading level="h2">{frontmatter.title}</Heading>
+          {content}
+        </div>
+        )) }
       </Container>
     </div>
   );
