1689afc19dd9e0038854a81117ec5500fd4cac26
Perry Harlock <perry@hussle.com>
2025-04-01 18:06:46 +0100

    mobile nav styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,22 @@
+'use client';
+import { MouseEvent, useState } from 'react';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { Container } from '@/components/Container';
 import { CustomLink } from '@/components/CustomLink';
 import { ButtonVariant } from './Button';
+import { twMerge } from 'tailwind-merge';
 
 export const Header = () => {
-  const mobileLinkClasses = 'px-2 py-4 no-underline';
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
+  const mobileLinkClasses = 'py-4 px-2 no-underline border-b-[1px] border-b-white focus:text-blue-900 focus:bg-white';
+
+  const handleNavigation = (event: MouseEvent<HTMLElement>) => {
+    console.log('hhhmmm');
+    event.preventDefault();
+    setIsNavigationOpen(!isNavigationOpen);
+  };
 
   const menuItems = [
     {
@@ -27,33 +38,37 @@ export const Header = () => {
       <a href="#main" className="sr-only">
         Skip to content
       </a>
-      <Container className="flex justify-between align-middle items-center relative">
+      {isNavigationOpen && <div onClick={(e) => handleNavigation(e)} className="fixed top-0 bottom-0 left-0 right-0 z-10 bg-blue-900 opacity-50 lg:hidden" />}
+      <Container className="flex justify-between align-middle items-center relative z-10">
         <Link href="/">
           <Image className="w-[137px] h-[80px] md:w-[202px] md:h-[118px]" src="/logo-new.svg" alt="Norton Studios Logo" width={604} height={307} priority />
         </Link>
-
-        <details className="lg:hidden">
-          <summary className="list-none text-xl cursor-pointer">
-            <svg width="38" height="38" aria-hidden="true" viewBox="0 0 24 24">
-              <path fill="#3B3752" d="M3 6h18v2H3zm0 5h18v2H3zm0 5h18v2H3z"></path>
-            </svg>
-            <span className="sr-only">Menu</span>
-          </summary>
-          <div className="absolute z-10 px-8 py-4 left-0 right-0 top-[80px] w-auto bg-tan-500">
-            <nav className="flex flex-col">
-              {menuItems.map((item) => {
-                return (
-                  <CustomLink key={item.url} variant="blue" href={item.url} className={mobileLinkClasses}>
-                    {item.label}
-                  </CustomLink>
-                );
-              })}
-            </nav>
-            <CustomLink href="/contact-us" asButtonVariant={ButtonVariant.SECONDARY}>
-              Talk to us!
-            </CustomLink>
-          </div>
-        </details>
+        <div>
+          <details open={isNavigationOpen} className="lg:hidden">
+            <summary
+              onClick={(e) => handleNavigation(e)}
+              className="list-none text-xl cursor-pointer border-2 border-white focus-visible:border-yellow-500 focus-visible:border-2 focus-visible:outline-none p-2"
+            >
+              <Image className={twMerge(isNavigationOpen && 'hidden')} src="/menu.svg" alt={''} width="24" height="24" />
+              <Image className={twMerge(!isNavigationOpen && 'hidden')} src="/menu-close.svg" alt={''} width="24" height="24" />
+              <span className="sr-only">{`${isNavigationOpen ? 'Close' : 'Open'} menu`}</span>
+            </summary>
+            <div className="absolute z-10 px-8 py-4 pb-8 left-0 right-0 top-[95px] lg:top-[130px] w-auto bg-tan-500">
+              <nav className="flex flex-col mb-4">
+                {menuItems.map((item) => {
+                  return (
+                    <CustomLink key={item.url} variant="blue" href={item.url} className={mobileLinkClasses}>
+                      {item.label}
+                    </CustomLink>
+                  );
+                })}
+              </nav>
+              <CustomLink href="/contact-us" asButtonVariant={ButtonVariant.SECONDARY}>
+                Talk to us!
+              </CustomLink>
+            </div>
+          </details>
+        </div>
 
         <div className="hidden lg:flex max-md:mt-6 max-md:py-6 max-md:border-t-2 max-md:border-t-slate-200 items-center">
           <nav>
